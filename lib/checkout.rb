0dfee5fe9d6dc7cd8746f84eaaf2093416cac709
jarkyn <jarkyn@ymail.com>
2015-05-02 13:35:42 +0100

    implements discount

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,15 +1,58 @@
+Item = Struct.new(:barcode, :price)
+
 class Checkout
-  def initialize
+
+  def initialize(promotions={})
     @items = []
+    @promotions = promotions
   end
-  
+
   def total
-    @items.reduce(0) { |total, item| total + item.price }
+    subtotal - discount
   end
 
   def scan(item)
     @items << item
   end
+
+  private
+  
+  def subtotal
+    @items.reduce(0) { |total, item| total + item.price }
+  end
+
+  def discount
+    tally.keys.reduce(0) do |discount, barcode|
+      discount + discount_per(barcode)
+    end
+  end
+
+  def tally
+    @items.reduce({}) do |memo, item|
+      memo[item.barcode] = memo.fetch(item.barcode, 0) + 1
+      memo
+    end
+  end
+
+  def discount_per(barcode)
+     discount_multiplier(barcode) * discount_amount(barcode)
+  end
+
+  def discount_multiplier(barcode)
+    (item_quantity(barcode) / discount_quantity(barcode)).floor
+  end
+
+  def discount_amount(barcode)
+    @promotions[barcode][:amount]
+  end
+
+  def discount_quantity(barcode)
+    @promotions[barcode][:quantity]
+  end
+
+  def item_quantity(barcode)
+    tally[barcode]
+  end
 end
 
-Item = Struct.new(:barcode, :price)
+
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,8 +1,11 @@
 require 'checkout'
 describe Checkout do
-  let(:checkout) {Checkout.new}
+  let(:checkout) {Checkout.new({
+                        'A' => {quantity: 3, amount: 20},
+                        'B' => {quantity: 2, amount: 15}
+                               })}
 
-  it 'nothing added to total if nothing scanned' do
+  it 'nothing scanned' do
     expect(checkout.total).to eq(0)
   end
 
@@ -11,4 +14,26 @@ describe Checkout do
     expect(checkout.total).to eq(50)
   end
 
+  
+  it 'discount not applicable' do
+    checkout.scan(Item.new('A', 50))
+    checkout.scan(Item.new('A', 50))
+    expect(checkout.total).to eq(100)
+  end
+
+  it 'discount is applicable' do
+    checkout.scan(Item.new('A', 50))
+    checkout.scan(Item.new('A', 50))
+    checkout.scan(Item.new('A', 50))
+    expect(checkout.total).to eq(130)
+  end
+
+  it 'multiple discounts applicable' do
+    checkout.scan(Item.new('A', 50))
+    checkout.scan(Item.new('A', 50))
+    checkout.scan(Item.new('A', 50))
+    checkout.scan(Item.new('B', 30))
+    checkout.scan(Item.new('B', 30))
+    expect(checkout.total).to eq(175)
+  end
 end
